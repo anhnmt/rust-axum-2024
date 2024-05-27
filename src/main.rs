25f8ac8c31a0482e4ba1f0295ad6dd9b11ce04a6
anhnmt <anhng.dev@gmail.com>
2024-05-27 17:15:38 +0700

    Add /verify route

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use axum::{Json, Router};
-use axum::http::StatusCode;
+use axum::http::{HeaderMap, StatusCode};
 use axum::routing::{get, post};
 use hashbrown::HashMap;
-use jsonwebtoken::{encode, EncodingKey, Header};
+use jsonwebtoken::{decode, DecodingKey, encode, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
@@ -17,7 +17,8 @@ async fn main() {
         .route("/", get(root))
         .route("/users", post(create_user))
         .route("/request", post(request))
-        .route("/login", post(login));
+        .route("/login", post(login))
+        .route("/verify", post(verify));
 
     // run our app with hyper, listening globally on port 3000
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -63,15 +64,34 @@ async fn login(
         iat: chrono::Utc::now().timestamp() as usize,
     };
 
-    let secret = "my_secret";
-
-    let token = encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref())).unwrap();
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret("my_secret".as_ref())
+    ).unwrap();
 
     let res = LoginResponse {
         access_token: token,
     };
 
     (StatusCode::OK, Json(Some(res)))
+}
+
+async fn verify(header_map: HeaderMap) -> Result<Json<String>, StatusCode> {
+    if let Some(token) = header_map.get("Authorization") {
+        let token = token.to_str().unwrap().replace("Bearer ", "");
+
+        return match decode::<Claims>(
+            &token,
+            &DecodingKey::from_secret("my_secret".as_ref()),
+            &Validation::default(),
+        ) {
+            Ok(token_data) => Ok(Json(token_data.claims.sub)),
+            Err(_) => Err(StatusCode::UNAUTHORIZED),
+        };
+    }
+
+    Err(StatusCode::UNAUTHORIZED)
 }
 
 #[derive(Deserialize)]
